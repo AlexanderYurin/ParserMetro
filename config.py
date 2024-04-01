1ebb0b9494446cab5e8d29321b83f83811b73dcf
Alexander Yurin <hiimmandarin32@gmail.com>
2024-04-01 21:33:10 +0300

    Config file

---
+++ config.py
@@ -0,0 +1,6 @@
+URL = "https://online.metro-cc.ru"
+HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) '
+						 'AppleWebKit/537.36 (KHTML, like Gecko) '
+						 'Chrome/105.0.0.0 Safari/537.36 '}
+
+QUANTITY_PRODUCT = 100
